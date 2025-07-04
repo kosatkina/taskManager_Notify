02c125a6c38c200aed746182332f276ce6ef2374
kosatkina <tatiana.kosatkina@gmail.com>
2025-07-04 12:25:25 -0400

    Added function to insert new record into the tables

--- tm_notify.py
+++ tm_notify.py
@@ -1,6 +1,9 @@
 import sqlite3
 from datetime import datetime
 
+# Constants
+TITLE_LEN = 21
+
 # Connect to SQLite DB
 conn = sqlite3.connect('notify.db')
 
@@ -24,6 +27,35 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS note (
 
 # Commit command
 conn.commit()
+
+# CRUD operations
+# Function to add a new note
+def create_note(content, title=None):
+
+    dt = datetime.now().strftime("%c")
+
+    # Generate title
+    if not title or title.strip() == "":
+        strippedTitle = content.strip()
+        if not strippedTitle:
+            title = "(Untitled Note)"
+        else:
+            if len(strippedTitle) > TITLE_LEN:
+                title = strippedTitle[:(TITLE_LEN - 3)] + "..."
+            else:
+                title = strippedTitle
+
+    # Insert record into note_list
+    cursor.execute("INSERT INTO note_list (title, date_created) VALUES (?, ?)", (title, dt))
+    id = cursor.lastrowid
+
+    # Insert into note
+    cursor.execute("INSERT INTO note (id, content) VALUES (?, ?)", (id, content))
+
+    conn.commit()
+    print(f"Added new note '{title}'")
+
+
 
 # Close connection
 conn.close()
