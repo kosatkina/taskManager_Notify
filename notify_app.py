183d54686d121f10ddf3d26b18c172830acb93bd
kosatkina <tatiana.kosatkina@gmail.com>
2025-07-17 14:18:52 -0400

    Reorganized file structure to increase readability and maintainability

--- notify_app.py
+++ notify_app.py
@@ -1,11 +1,15 @@
 ## This file is the main entry point of the Notify App
 
 from PyQt6.QtWidgets import QApplication
-from main_app_window import MainAppWindow
+from ui.main_app_window import MainAppWindow
+from db.database import init_db
 import sys
 
 
 if __name__ == "__main__":
+    # Initialize the database before GUI loads
+    init_db()
+    
     notify_app = QApplication(sys.argv)
     window = MainAppWindow()
     window.show()
