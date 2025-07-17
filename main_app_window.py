1145e49906af62e0359a47ab889e2ee6ad9dff04
kosatkina <tatiana.kosatkina@gmail.com>
2025-07-17 11:29:03 -0400

    Added create_menu function with slots for Add, list and Delete signals

--- main_app_window.py
+++ main_app_window.py
@@ -1,55 +1,91 @@
-from PyQt6.QtWidgets import QMainWindow, QMessageBox
+from PyQt6.QtWidgets import (
+    QApplication, QMainWindow, QTextEdit, QMenuBar, 
+    QMenu, QMessageBox, QInputDialog, QStatusBar
+)
 from PyQt6.QtGui import QAction
+from PyQt6.QtCore import Qt
+
+MAX_OPEN_WINDOWS = 5
+MAX_CHARACTERS = 500
 
 
 class MainAppWindow(QMainWindow):
+    open_windows = []
+
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("Notify")
-        self.setGeometry(1150, 100, 250, 250) 
+        #self.setGeometry(1150, 100, 250, 250) 
+        self.setFixedSize(300, 300)
 
-        # TODO - Set icon
+        # TODO - Set icons
 
-        self._init_menu()
-        self.statusBar().showMessage("Max 500 characters.")
-
-        def _init_menu(self):
-            menu_bar = self.menuBar()
-
-            # Notes menu
-            note_menu = menu_bar.addMenu
-
-
-                # Note Storage - CHANGE TO DB
-        self.notes = []
-
-        # Central widget and layout
-        central_widget = QWidget()
-        layout = QVBoxLayout()
-
-        self.text_edit = QTextEdit("")
+        # Central widget
+        self.text_edit = QTextEdit()
+        self.text_edit.textChanged.connect(self.update_status)
+        self.setCentralWidget(self.text_edit)
         
-        self.add_note_btn = QPushButton("Add")
-        self.add_note_btn.clicked.connect(self.add_note_btn_clicked)
+        # Menu and status bar
+        self.create_menu()
+        self.create_status()
         
-        self.note_list_btn = QPushButton("List")
-        self.note_list_btn.clicked.connect(self.note_list_btn_clicked)
 
-        layout.addWidget(self.text_edit)
-        layout.addWidget(self.add_note_btn)
-        layout.addWidget(self.note_list_btn)
+    # Function to create menu
+    def create_menu(self):
+        menu_bar = self.menuBar()
 
-        central_widget.setLayout(layout)
-        self.setCentralWidget(central_widget)
+        note_menu = menu_bar.addMenu("Menu")    # TODO - Change to icon
+
+        add_action = QAction("Add", self)
+        add_action.triggered.connect(self.add_note)
+
+        list_action = QAction("List", self)
+        list_action.triggered.connect(self.list_notes)
+
+        delete_action = QAction("Delete", self)
+        delete_action.triggered.connect(self.delete_note)
+
+        note_menu.addAction(add_action)
+        note_menu.addAction(list_action)
+        note_menu.addAction(delete_action)
 
     # Callback function on Add note button click signal
     # This function opens new main window above existing one
-    def add_note_btn_clicked(self):
+    def add_note(self):
+            # Max 5 notes can be opened 
+            if len(MainAppWindow.open_windows) >= MAX_OPEN_WINDOWS:
+                 QMessageBox.warning(self, "Limit reached", " Max five notes.")
+                 return
+            
+            self.new_window = MainAppWindow()
+            self.new_window.show()
+    
+    # Callback function to list all notes
+    def list_notes(self):
+        note_titles = [f"Note {i+1}" for i in range(len(MainAppWindow.open_windows))]
+        selected, ok = QInputDialog.getItem(self, "List Notes", "Select a note to view:", note_titles, 0, False)
+        if ok:
+            index = note_titles.index(selected)
+            MainAppWindow.open_windows[index].raise_()
+            MainAppWindow.open_windows[index].activateWindow()
+
+    # Callback function to delete the note
+    def delete_note(self):
+         confirm = QMessageBox.question(self, "Delete", "Are you sure you want to delete this note?")
+         
+         if confirm == QMessageBox.StandardButton.Yes:
+              MainAppWindow.open_windows.remove(self)
+              self.close()
+    
+
+    # Function to create status bar
+    def create_status(self):
+         pass
+
+    # Callback function to update status bar info
+    def update_status(self):
         pass
 
-    # Callback function on Menu button click signal
-    # This function opens drop-down menu 
-    def note_list_btn_clicked(self):
-        pass
+
 
